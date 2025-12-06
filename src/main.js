8f09eea9a224606d7d9001948246f4a937970311
Vic <125237471+vicsanity623@users.noreply.github.com>
2025-12-06 00:53:35 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Global Instances
-const VERSION = 'v6.6.0.2';
+const VERSION = 'v6.6.0.3';
 const player = new Player();
 const world = new World(Date.now());
 const canvas = document.getElementById('gameCanvas');
