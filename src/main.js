4e58a9e5b17a31c31f2acab207a965eea36d2f45
Vic <vic@Vics-iMac.local>
2025-12-06 03:40:59 -0700

    fix3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Global Instances
-const VERSION = 'v7.3';
+const VERSION = 'v7.4';
 const player = new Player();
 const world = new World(Date.now());
 const canvas = document.getElementById('gameCanvas');
